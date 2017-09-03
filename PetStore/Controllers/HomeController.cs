92ccae292b3cf5621219413dfca5ec2fa9b40d7e
HOME\Coolio <jkerny81@gmail.com>
2017-09-03 13:41:20 +1000

    Fixing up scrolling pagination

--- PetStore/Controllers/HomeController.cs
+++ PetStore/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using PetStore.Repository;
+﻿using PetStore.Models.Pet;
+using PetStore.Repository;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -22,9 +23,22 @@ namespace PetStore.Controllers
         [HttpGet]
         public ActionResult GetPaginatedPetData(int pageIndex, int pageSize)
         {
-            var pets = _petService.GetAllPets().Skip(pageIndex * pageSize).Take(pageSize).ToList();            
-          
-            return Json(pets, JsonRequestBehavior.AllowGet);
+            var pets = _petService.GetAllPets().Skip(pageIndex * pageSize).Take(pageSize).ToList();
+
+            var model = new List<PetData>();
+            foreach (var pet in pets)
+            {
+                model.Add(new PetData
+                {
+                    Name = pet.Name,
+                    DateOfBirth = pet.DateOfBirth,
+                    AnimalType = pet.AnimalType.Name,
+                    Weight = pet.Weight
+                }
+                );
+            }
+
+            return Json(model, JsonRequestBehavior.AllowGet);
         }
     }
 }
---
+++ PetStore/Models/Pet/PetData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PetStore.Models.Pet
+{
+    public class PetData
+    {
+        public string Name { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string AnimalType { get; set; }
+        public double Weight { get; set; }
+    }
+}
